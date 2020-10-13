1116a8dea788edb3417c39c98ef3bd985b3f3e17
Iñigo Esteban <iestebanmarina@gmail.com>
2020-10-13 09:21:05 +0200

    Updated [201013]

--- session1/a_word_cloud.py
+++ session1/a_word_cloud.py
@@ -24,21 +24,22 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 # Create a list of word
-text = ("Python Python Python Matplotlib Matplotlib Seaborn Network Plot Violin Chart Pandas Datascience Wordcloud Spider Radar Parrallel Alpha Color Brewer Density Scatter Barplot Barplot Boxplot Violinplot Treemap Stacked Area Chart Chart Visualization Dataviz Donut Pie Time-Series Wordcloud Wordcloud Sankey Bubble")
-
+# text = ("Python Python Python Matplotlib Matplotlib Seaborn Network Plot Violin Chart Pandas Datascience Wordcloud Spider Radar Parrallel Alpha Color Brewer Density Scatter Barplot Barplot Boxplot Violinplot Treemap Stacked Area Chart Chart Visualization Dataviz Donut Pie Time-Series Wordcloud Wordcloud Sankey Bubble")
+# text = ("Patricia te quiero mucho. Mamá muchísimas felicidades. Cris mucho ánimo, venderás muchas obras")
 # Create the wordcloud object
-wordcloud = WordCloud(width=480, height=480, margin=0).generate(text)
+# wordcloud = WordCloud(width=480, height=480, margin=0).generate(text)
 
 # Display the generated image:
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.margins(x=0, y=0)
-plt.show()
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis("off")
+# plt.margins(x=0, y=0)
+# plt.show()
 
 
 # Let's do it using a text file
 
-text = open('../data/wikipedia_opendata.txt').read()
+text = open('../data/aaa.txt').read()
+#text = open('../data/wikipedia_opendata.txt').read()
 text = text.upper()
 
 # Create the wordcloud object
