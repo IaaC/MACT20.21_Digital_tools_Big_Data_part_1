1621469a08e67501541b725c4488a9f8e5e99531
acerezoc <alvarocc05@hotmail.com>
2020-11-02 19:06:58 +0100

    First python class
    Data plotting & personal exercises

--- alvarocerezo/Lux_statistics.py
+++ alvarocerezo/Lux_statistics.py
@@ -26,21 +26,21 @@ a_df = pd.read_csv('../alvarocerezo/dataexport_20201102T170744.csv')
 
 # Range is the combination of min-max values
 print('####################')
-print('This is an example of the range values for population in Catalunya:')
+print('This is an example of the range values for temperature in Luxembourg:')
 lux = a_df['Luxembourg_Temperature']
 min_value = lux.min()
 max_value = lux.max()
-print('Population ranged between %i and %i' % (min_value, max_value))
+print('Temperature ranged between %i and %i' % (min_value, max_value))
 
 # There are a set of statistics to descibe central tendency
 mean_value = lux.mean()
 mode_value = lux.mode()
 median_value = lux.median()
 print('####################')
-print('The three values for the central tendency of population in Catalunya are:')
-print('Mean population: %i' % mean_value)
-print('Mode population: %s' % str(mode_value))
-print('Median population: %i' % median_value)
+print('The three values for the central tendency of temperature in Luxembourg are:')
+print('Mean temperature: %i' % mean_value)
+print('Mode temperature: %s' % str(mode_value))
+print('Median temperature: %i' % median_value)
 
 
 
@@ -48,7 +48,7 @@ print('Median population: %i' % median_value)
 variance_value = lux.var()
 stdev_value = lux.std()
 print('####################')
-print('The two values for the central tendency of population in Catalunya are:')
+print('The two values for the central tendency of temperature in Luxembourg are:')
 print('Variance: %f' % variance_value)
 print('Standard deviation: %f' % stdev_value)
 
---
+++ alvarocerezo/lux_temperature_plot.py
@@ -0,0 +1,55 @@
+# encoding: utf-8
+
+##################################################
+# This script shows uses the pandas library to create statistically describe data sets
+# It also shows basic plotting features
+# Find extra documentation about data frame here:
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html
+##################################################
+#
+##################################################
+# Author: Diego Pajarito
+# Copyright: Copyright 2019, IAAC
+# Credits: [Institute for Advanced Architecture of Catalonia - IAAC, Advanced Architecture group]
+# License:  Apache License Version 2.0
+# Version: 1.0.0
+# Maintainer: Diego Pajarito
+# Email: diego.pajarito@iaac.net
+# Status: development
+##################################################
+
+# We need to import pandas library as well as the plot libraries matplotlib and seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+# We read the file for population data
+a_df = pd.read_csv('../alvarocerezo/dataexport_20201102T170744.csv')
+
+# Range is the combination of min-max values
+print('####################')
+print('This is an example of range values for temperature in Luxembourg:')
+pop = a_df['Luxembourg_Temperature']
+
+
+# Histogram and normal distribution
+sns.distplot(pop)
+plt.show()
+
+# Kernel density
+sns.distplot(pop, hist=False, rug=True)
+plt.show()
+
+# A kernel density estimate (KDE) plot
+sns.kdeplot(pop, shade=True)
+plt.show()
+
+
+# Scatter plot comparing areas and populations
+a_df.plot.scatter(x='Luxembourg_Precipitation', y='Luxembourg_Temperature', c='DarkBlue')
+plt.show()
+
+print('End')
+
+# Try to compare multiple countries
